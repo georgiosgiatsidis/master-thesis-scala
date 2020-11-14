38d6481a7716d3435e9aecdf5e5a3df29933f514
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-11-15 01:11:12 +0200

    Use slick instead of low level jdbc

--- src/main/scala/com/giatsidis/spark/services/MysqlService.scala
+++ src/main/scala/com/giatsidis/spark/services/MysqlService.scala
@@ -1,63 +1,47 @@
 package com.giatsidis.spark.services
 
-import java.sql.{DriverManager, PreparedStatement}
-
-import com.giatsidis.spark.Config
-import com.giatsidis.spark.Tables.{Tweets, User, Users}
+import com.giatsidis.spark.Tables.{Tweets, Users, Tweet => TablesTweet, User => TablesUsers}
 import com.giatsidis.spark.models.Tweet
 import org.apache.log4j.Logger
+import org.apache.spark.rdd.RDD
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.Await
 import slick.jdbc.MySQLProfile.api._
 
-object MysqlService extends Serializable {
+object MysqlService {
 
-  @transient lazy val log = Logger.getLogger(this.getClass)
+  val log = Logger.getLogger(this.getClass)
 
-  def save(partition: Iterator[Tweet]) = {
+  def save(rdd: RDD[Tweet]) = {
+    rdd.foreachPartition(partition => {
+      val db = Database.forConfig("mysql")
 
-    Class.forName("com.mysql.jdbc.Driver")
-    val connection = DriverManager.getConnection(
-      s"jdbc:mysql://${Config.dbHost}:${Config.dbPort}/${Config.dbName}",
-      Config.dbUsername,
-      Config.dbPassword
-    )
-
-    val sql = "INSERT INTO tweets (id, full_text, location, sentiment, created_at, user_id) VALUES (?, ?, ?, ?, ?, ?)"
-    val preparedStatement: PreparedStatement = connection.prepareStatement(sql)
-    val db = Database.forConfig("mysql")
-
-    try {
-      connection.setAutoCommit(false)
-      partition.foreach(record => {
-
-        val f = {
-          val users: TableQuery[Users] = TableQuery[Users]
-          val updated = users.insertOrUpdate(User(record.user.id, record.user.screenName, record.user.profileImageURLHttps))
-          db.run(updated)
+      try {
+        partition.foreach {
+          record => {
+            val users = TableQuery[Users].insertOrUpdate(TablesUsers(record.user.id, record.user.screenName, record.user.profileImageURLHttps))
+            Await.result(db.run(users), Duration.Inf)
+            val tweets = TableQuery[Tweets].insertOrUpdate(
+              TablesTweet(
+                record.id,
+                record.fullText,
+                record.location,
+                record.sentiment,
+                record.createdAt,
+                Option(record.user.id)
+              )
+            )
+            Await.result(db.run(tweets), Duration.Inf)
+          }
         }
-        Await.result(f, Duration.Inf)
-
-        preparedStatement.setLong(1, record.id)
-        preparedStatement.setString(2, record.fullText)
-        preparedStatement.setString(3, if (record.location.isEmpty) null else record.location.get)
-        preparedStatement.setString(4, record.sentiment)
-        preparedStatement.setString(5, record.createdAt)
-        preparedStatement.setLong(6, record.user.id)
-
-        preparedStatement.addBatch()
-      })
-      preparedStatement.executeBatch()
-      connection.commit
-    } catch {
-      case e: Exception =>
-        log.error(e)
-    } finally {
-      connection.close()
-      preparedStatement.close()
-      db.close
-    }
+      } catch {
+        case e: Exception =>
+          log.error(e)
+      } finally {
+        db.close
+      }
+    })
   }
 
 }
