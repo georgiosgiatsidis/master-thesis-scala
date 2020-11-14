5e9f16915dc88ee26aed776e0c70a2cc57e9c8b4
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-11-14 22:38:01 +0200

    Describe the User Schema

--- src/main/scala/com/giatsidis/spark/Tables.scala
+++ src/main/scala/com/giatsidis/spark/Tables.scala
@@ -1,12 +1,29 @@
 package com.giatsidis.spark
 
-import com.giatsidis.spark.models.Tweet
 import slick.jdbc.JdbcProfile
-import slick.lifted.ProvenShape
+import slick.lifted.{ForeignKeyQuery, ProvenShape}
 
 object Tables extends JdbcProfile {
 
   import api._
+
+  case class User(id: Long, screenName: String, profileImageURLHttps: String)
+
+  class Users(tag: Tag) extends Table[User](tag, "users") {
+
+    def id: Rep[Long] = column[Long]("id", O.PrimaryKey)
+
+    def screenName: Rep[String] = column[String]("screen_name")
+
+    def profileImageURLHttps: Rep[String] = column[String]("profile_image_url_https")
+
+    def * : ProvenShape[User] =
+      (id, screenName, profileImageURLHttps) <> (User.tupled, User.unapply)
+  }
+
+  val users = TableQuery[Users]
+
+  case class Tweet(id: Long, fullText: String, location: Option[String] = None, sentiment: String, createdAt: String, userId: Long)
 
   class Tweets(tag: Tag) extends Table[Tweet](tag, "tweets") {
 
@@ -20,8 +37,15 @@ object Tables extends JdbcProfile {
 
     def createdAt: Rep[String] = column[String]("created_at")
 
+    def userId: Rep[Long] = column[Long]("user_id")
+
+    def user: ForeignKeyQuery[Users, User] =
+      foreignKey("user_fk", userId, users)(_.id, onUpdate = ForeignKeyAction.Restrict, onDelete = ForeignKeyAction.Cascade)
+
     def * : ProvenShape[Tweet] =
-      (id, fullText, location.?, sentiment, createdAt) <> (Tweet.tupled, Tweet.unapply)
+      (id, fullText, location.?, sentiment, createdAt, userId) <> (Tweet.tupled, Tweet.unapply)
   }
+
+  val tweets = TableQuery[Tweets]
 
 }
