eba2a61adbf594e7083b306294dd2a20a3c83277
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-11-14 23:34:04 +0200

    Insert or update user

--- src/main/scala/com/giatsidis/spark/services/MysqlService.scala
+++ src/main/scala/com/giatsidis/spark/services/MysqlService.scala
@@ -3,14 +3,20 @@ package com.giatsidis.spark.services
 import java.sql.{DriverManager, PreparedStatement}
 
 import com.giatsidis.spark.Config
+import com.giatsidis.spark.Tables.{Tweets, User, Users}
 import com.giatsidis.spark.models.Tweet
-import org.apache.log4j.{Logger}
+import org.apache.log4j.Logger
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
+import slick.jdbc.MySQLProfile.api._
 
 object MysqlService extends Serializable {
 
   @transient lazy val log = Logger.getLogger(this.getClass)
 
   def save(partition: Iterator[Tweet]) = {
+
     Class.forName("com.mysql.jdbc.Driver")
     val connection = DriverManager.getConnection(
       s"jdbc:mysql://${Config.dbHost}:${Config.dbPort}/${Config.dbName}",
@@ -18,16 +24,27 @@ object MysqlService extends Serializable {
       Config.dbPassword
     )
 
-    val sql = "INSERT INTO tweets (id, full_text, location, sentiment, created_at) VALUES (?, ?, ?, ?, ?)"
+    val sql = "INSERT INTO tweets (id, full_text, location, sentiment, created_at, user_id) VALUES (?, ?, ?, ?, ?, ?)"
     val preparedStatement: PreparedStatement = connection.prepareStatement(sql)
+    val db = Database.forConfig("mysql")
+
     try {
       connection.setAutoCommit(false)
       partition.foreach(record => {
+
+        val f = {
+          val users: TableQuery[Users] = TableQuery[Users]
+          val updated = users.insertOrUpdate(User(record.user.id, record.user.screenName, record.user.profileImageURLHttps))
+          db.run(updated)
+        }
+        Await.result(f, Duration.Inf)
+
         preparedStatement.setLong(1, record.id)
         preparedStatement.setString(2, record.fullText)
         preparedStatement.setString(3, if (record.location.isEmpty) null else record.location.get)
         preparedStatement.setString(4, record.sentiment)
         preparedStatement.setString(5, record.createdAt)
+        preparedStatement.setLong(6, record.user.id)
 
         preparedStatement.addBatch()
       })
@@ -39,6 +56,7 @@ object MysqlService extends Serializable {
     } finally {
       connection.close()
       preparedStatement.close()
+      db.close
     }
   }
 
